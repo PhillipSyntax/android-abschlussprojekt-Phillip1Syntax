1fd5fb9f4d623b30ab1bc42b54d4f8b739efa9a4
phillipwilke <phillipwilke1@web.de>
2024-05-30 21:21:57 +0200

    add val results and resultInput add three function setSelectedProduct loadResultInput updateInputText

--- app/src/main/java/de/phillipw/moebelhawi/kotlin/HomeViewModel.kt
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/HomeViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import de.phillipw.moebelhawi.kotlin.data.Repository
+import de.phillipw.moebelhawi.kotlin.data.models.Product
 import de.phillipw.moebelhawi.kotlin.remote.MöbelHawiApi
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
@@ -23,17 +24,41 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
     val topRated = repository.topRated
 
+    val results = repository.results
 
+
+
+    private val _resultInput = MutableLiveData<String>()
+
+    val resultInput: LiveData<String>
+        get() = _resultInput
+
+
+    private val _selectedProduct = MutableLiveData<Product>()
+
+
+    val selectedProduct : LiveData<Product>
+        get() = _selectedProduct
 
     fun loadTopSellers() {
         viewModelScope.launch {
             repository.getTopSellers()
         }
     }
-
     fun loadTopRated() {
         viewModelScope.launch {
             repository.getTopRated()
         }
+    }
+    fun setSelectedProduct(product: Product) {
+        _selectedProduct.value = product
+    }
+    fun loadResultInput(query: String) {
+        viewModelScope.launch {
+            repository.getResultSearch(query)
+        }
+    }
+    fun updateInputText(query: String) {
+        _resultInput.value = query
     }
 }
