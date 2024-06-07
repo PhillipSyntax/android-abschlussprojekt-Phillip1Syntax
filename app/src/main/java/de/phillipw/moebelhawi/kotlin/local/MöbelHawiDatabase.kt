03675ab30a442a567837cb627d762f126ba0c15f
phillipwilke <phillipwilke1@web.de>
2024-06-07 15:14:30 +0200

    change Database one abstract val for CartDao and another one for ProductDao

---
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/local/MöbelHawiDatabase.kt
@@ -0,0 +1,32 @@
+package de.phillipw.moebelhawi.kotlin.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import de.phillipw.moebelhawi.kotlin.data.models.Product
+import de.phillipw.moebelhawi.kotlin.data.models.ShoppingCartItem
+
+
+@Database(entities = [Product::class,ShoppingCartItem::class], version = 1)
+abstract class MöbelHawiDatabase : RoomDatabase(){
+
+    abstract val itemCartDao: CartDao
+    abstract val productDao: ProductDao
+
+    companion object {
+
+        private lateinit var dbInstance: MöbelHawiDatabase
+
+        fun getDatabase(context: Context): MöbelHawiDatabase {
+            if (!this::dbInstance.isInitialized) {
+                dbInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MöbelHawiDatabase::class.java,
+                    "products_database"
+                ).build()
+            }
+            return dbInstance
+        }
+    }
+}
