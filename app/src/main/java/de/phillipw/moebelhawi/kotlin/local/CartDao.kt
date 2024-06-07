a7df938e7e0db8444b0ca76db29218ca7705303a
phillipwilke <phillipwilke1@web.de>
2024-06-07 15:05:55 +0200

    Create CartDao

---
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/local/CartDao.kt
@@ -0,0 +1,30 @@
+package de.phillipw.moebelhawi.kotlin.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import de.phillipw.moebelhawi.kotlin.data.models.ShoppingCartItem
+
+/** FÜr den Warenkorb*/
+@Dao
+interface CartDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCart(shoppingCartItem: ShoppingCartItem)
+
+    @Update
+    suspend fun updateCart(shoppingCartItem: ShoppingCartItem) // Kann ein Update für Beide verwendet werden ?
+
+    @Query("SELECT * FROM shopping_card_item")
+    fun getCartItems(): LiveData<List<ShoppingCartItem>>
+
+    @Query("DELETE FROM shopping_card_item WHERE id = :id")
+    suspend fun deleteById(id: Int)
+
+    @Query("DELETE FROM shopping_card_item")
+    fun clearShoppingCart()
+
+}
