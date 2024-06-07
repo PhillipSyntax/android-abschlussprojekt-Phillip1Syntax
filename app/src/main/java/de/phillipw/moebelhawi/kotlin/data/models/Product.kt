714a2915297bd0c19c7c644679f72b20ab952cfa
phillipwilke <phillipwilke1@web.de>
2024-06-07 15:16:19 +0200

    add TypeConverter for UrlListConverter

--- app/src/main/java/de/phillipw/moebelhawi/kotlin/data/models/Product.kt
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/data/models/Product.kt
@@ -1,14 +1,24 @@
 package de.phillipw.moebelhawi.kotlin.data.models
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.squareup.moshi.Json
+import de.phillipw.moebelhawi.kotlin.data.util.UrlListConverter
+import de.phillipw.moebelhawi.kotlin.remote.BASE_URL
 
-@Entity(tableName = "product_table")
-
+@Entity(tableName = "products")
+@TypeConverters(UrlListConverter::class)
 data class Product(
     @Json(name = "product_id")
+    @ColumnInfo("product_id")
     val productId: String,
     val title: String,
+    @Ignore
     val thumbnails: List<List<String>>,
-    val price: Double
+    val price: Double,
+    val thumbnailsSmall: List<String>,
+    val thumbnailsBig: List<String>
 )
