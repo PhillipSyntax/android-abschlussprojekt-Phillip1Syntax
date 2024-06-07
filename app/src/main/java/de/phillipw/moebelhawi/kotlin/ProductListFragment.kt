1146bc9f0a33bd76d569823824cbbfac7da48603
phillipwilke <phillipwilke1@web.de>
2024-06-07 15:17:30 +0200

    Create ProductListFragment

---
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/ProductListFragment.kt
@@ -0,0 +1,49 @@
+package de.phillipw.moebelhawi.kotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import de.phillipw.moebelhawi.kotlin.adapter.ProductListAdapter
+import de.phillipw.moebelhawi.kotlin.databinding.FragmentProductListBinding
+
+
+class ProductListFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductListBinding
+
+    private val viewModel: HomeViewModel by activityViewModels()
+
+    private val args: ProductListFragmentArgs by navArgs()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentProductListBinding.inflate(layoutInflater)
+
+        viewModel.loadByCategorie(args.title)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvResults.text = args.title
+        viewModel.categorieResults.observe(viewLifecycleOwner) {
+            binding.rvProductlist.adapter = ProductListAdapter(it){
+                viewModel.setSelectedProduct(it)
+                findNavController().navigate(R.id.productDetailFragment)
+            }
+        }
+
+    }
+
+
+}
