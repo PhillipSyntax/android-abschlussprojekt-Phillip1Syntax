83a1c1009487bcc5fa08e589aea64203b35775df
phillipwilke <phillipwilke1@web.de>
2024-06-10 21:35:20 +0200

    add PrimaryKey and constructor

--- app/src/main/java/de/phillipw/moebelhawi/kotlin/data/models/Product.kt
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/data/models/Product.kt
@@ -3,6 +3,7 @@ package de.phillipw.moebelhawi.kotlin.data.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.squareup.moshi.Json
@@ -12,13 +13,29 @@ import de.phillipw.moebelhawi.kotlin.remote.BASE_URL
 @Entity(tableName = "products")
 @TypeConverters(UrlListConverter::class)
 data class Product(
+    @PrimaryKey
     @Json(name = "product_id")
     @ColumnInfo("product_id")
     val productId: String,
     val title: String,
     @Ignore
-    val thumbnails: List<List<String>>,
+    val thumbnails: List<List<String>> = emptyList(),
     val price: Double,
-    val thumbnailsSmall: List<String>,
-    val thumbnailsBig: List<String>
-)
+    val thumbnailsSmall: List<String> = emptyList(),
+    val thumbnailsBig: List<String> = emptyList()
+) {
+    constructor(
+        productId: String,
+        title: String,
+        price: Double,
+        thumbnailsSmall: List<String>,
+        thumbnailsBig: List<String>
+    ) : this(
+        productId,
+        title,
+        emptyList(),
+        price,
+        thumbnailsSmall,
+        thumbnailsBig
+    )
+}
