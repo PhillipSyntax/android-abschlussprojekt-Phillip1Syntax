b4abf2670634f2696d1684efcfe4a5b723e7b3b2
phillipwilke <phillipwilke1@web.de>
2024-06-11 19:46:26 +0200

    add totalPrice for sum the article change name

--- app/src/main/java/de/phillipw/moebelhawi/kotlin/HomeViewModel.kt
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/HomeViewModel.kt
@@ -7,25 +7,37 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import de.phillipw.moebelhawi.kotlin.data.Repository
 import de.phillipw.moebelhawi.kotlin.data.models.Product
+import de.phillipw.moebelhawi.kotlin.data.models.ShoppingCartItem
+import de.phillipw.moebelhawi.kotlin.local.MöbelHawiDatabase
 import de.phillipw.moebelhawi.kotlin.remote.MöbelHawiApi
 import kotlinx.coroutines.launch
 import org.jetbrains.annotations.ApiStatus
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: Repository = Repository(MöbelHawiApi)
+    private val database = MöbelHawiDatabase.getDatabase(application)
+
+    private val repository: Repository = Repository(MöbelHawiApi, database)
 
     private val _loading = MutableLiveData<ApiStatus>()
+
+    private val _totalPrice = MutableLiveData<Double>()
+
+    val totalPrice: LiveData<Double>
+        get() = _totalPrice
 
     val loading: LiveData<ApiStatus>
         get() = _loading
 
-    val topSellers = repository.topSellers
+    val topSellers = repository.topSellerProducts
 
-    val topRated = repository.topRated
+    val topRated = repository.topRatedProducts
 
-    val results = repository.results
+    val results = repository.searchResultProducts
 
+    val categoryResults = repository.categoryResults
+
+    val cartItems = repository.shoppingCartItems
 
 
     private val _resultInput = MutableLiveData<String>()
@@ -37,28 +49,80 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private val _selectedProduct = MutableLiveData<Product>()
 
 
-    val selectedProduct : LiveData<Product>
+    val selectedProduct: LiveData<Product>
         get() = _selectedProduct
+
+
 
     fun loadTopSellers() {
         viewModelScope.launch {
             repository.getTopSellers()
         }
     }
+
     fun loadTopRated() {
         viewModelScope.launch {
             repository.getTopRated()
         }
     }
+
     fun setSelectedProduct(product: Product) {
         _selectedProduct.value = product
     }
+
     fun loadResultInput(query: String) {
         viewModelScope.launch {
-            repository.getResultSearch(query)
+            repository.getProductsBySearchQuery(query)
         }
     }
+
     fun updateInputText(query: String) {
         _resultInput.value = query
+    }
+
+    fun loadByCategory(query: String) {
+        viewModelScope.launch {
+            repository.getProductsByCategory(query)
+        }
+    }
+
+    fun getCartItems() {
+        viewModelScope.launch {
+            repository.getShoppingCartItems()
+        }
+    }
+
+    fun addToShoppingCart(product: Product) {
+
+        val shoppingCartItem = ShoppingCartItem(0,product.productId,1, product.price)
+        viewModelScope.launch {
+            repository.insertShoppingCartItem(shoppingCartItem)
+
+        }
+    }
+    fun deleteItemFromShoppingCart(shoppingCartItem: ShoppingCartItem) {
+        viewModelScope.launch {
+            repository.deleteItem(shoppingCartItem.id)
+            updateCartTotal()
+        }
+    }
+        fun updateCartItem(item: ShoppingCartItem) {
+            viewModelScope.launch {
+                repository.updateItem(item)
+
+            }
+        }
+    fun updateCartTotal() {
+        viewModelScope.launch {
+            repository.getShoppingCartItems()
+        }
+    }
+    fun calculateTotalPrice() {
+        viewModelScope.launch {
+            val items = repository.getShoppingCartItems()
+            //Hier hänge ich fest
+
+        }
+
     }
 }
