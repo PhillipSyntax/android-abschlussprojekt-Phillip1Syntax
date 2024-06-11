5eaf1ff26fa52711b8547465ea0348dc381f620c
phillipwilke <phillipwilke1@web.de>
2024-06-11 19:51:20 +0200

    create CheckoutFragment not finished yet

---
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/ui/CheckoutFragment.kt
@@ -0,0 +1,36 @@
+package de.phillipw.moebelhawi.kotlin.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import de.phillipw.moebelhawi.kotlin.HomeViewModel
+import de.phillipw.moebelhawi.kotlin.databinding.FragmentCheckoutBinding
+
+
+class CheckoutFragment : Fragment() {
+
+    private lateinit var binding: FragmentCheckoutBinding
+
+    private val viewModel: HomeViewModel by activityViewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentCheckoutBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+}
