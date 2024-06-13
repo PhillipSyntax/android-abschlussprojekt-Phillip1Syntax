111da6144f316d02d117c0fcbf122ae5ed286a3e
phillipwilke <phillipwilke1@web.de>
2024-06-13 22:24:28 +0200

    coorect build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 buildscript {
     repositories {
         google()
+            mavenCentral()
 
     }
     dependencies {
         val navVersion = "2.7.5"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+
     }
 }
 
 plugins {
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("androidx.room") version "2.6.1" apply false // Ist es so korrekt ?
