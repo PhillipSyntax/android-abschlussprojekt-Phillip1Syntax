199df0db0af9e46bac8ef6480cf83ac7941b6c1c
phillipwilke <phillipwilke1@web.de>
2024-06-16 23:16:48 +0200

    add funtcion to check the contents the ShoppingCart

--- app/src/main/java/de/phillipw/moebelhawi/kotlin/ui/ShoppingCartFragment.kt
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/ui/ShoppingCartFragment.kt
@@ -1,9 +1,11 @@
 package de.phillipw.moebelhawi.kotlin.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -39,12 +41,20 @@ class ShoppingCartFragment : Fragment() {
                 viewModel.updateCartItem(it)
             }
         }
-        viewModel.totalPrice.observe(viewLifecycleOwner) {totalPrice ->
+        viewModel.totalPrice.observe(viewLifecycleOwner) { totalPrice ->
             binding.tvTotalPrice.text = "Total Price: %.2f €".format(totalPrice)
         }
-        binding.btnCheckout.setOnClickListener{
-            val action = ShoppingCartFragmentDirections.cartToCheckout()
-            findNavController().navigate(action)
+        binding.btnCheckout.setOnClickListener {
+                if (viewModel.cartItems.value.isNullOrEmpty()) {
+                    Toast.makeText(
+                        requireContext(),
+                        "no items, please check your Cart",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val action = ShoppingCartFragmentDirections.cartToCheckout()
+                    findNavController().navigate(action)
+                }
+            }
         }
     }
-}
