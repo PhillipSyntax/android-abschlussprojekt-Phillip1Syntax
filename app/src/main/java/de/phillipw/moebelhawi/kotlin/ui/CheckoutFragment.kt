846ce4fb3f505004fa5a6c0040557dd9ef2ecc03
phillipwilke <phillipwilke1@web.de>
2024-06-16 23:07:00 +0200

    add if/else for check field first/second and zip

--- app/src/main/java/de/phillipw/moebelhawi/kotlin/ui/CheckoutFragment.kt
+++ app/src/main/java/de/phillipw/moebelhawi/kotlin/ui/CheckoutFragment.kt
@@ -1,12 +1,16 @@
 package de.phillipw.moebelhawi.kotlin.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import de.phillipw.moebelhawi.kotlin.HomeViewModel
+import de.phillipw.moebelhawi.kotlin.R
 import de.phillipw.moebelhawi.kotlin.adapter.CheckoutAdapter
 import de.phillipw.moebelhawi.kotlin.databinding.FragmentCheckoutBinding
 
@@ -41,5 +45,61 @@ class CheckoutFragment : Fragment() {
 
 
         }
+
+        binding.btnConfirm.setOnClickListener {
+
+            val correctName = "^[a-zA-Z ]+$".toRegex()
+
+            val correctPlz = "^[0-9]{5}$".toRegex()
+
+            val firstName = binding.firstNameEt.text.toString()
+            val secondName = binding.secondNameEt.text.toString()
+            val adress = binding.addressEt.text.toString()
+            val place = binding.placeEt.text.toString()
+            val zip = binding.zipEt.text.toString()
+            if (firstName.isNotBlank() && firstName.matches(correctName) && secondName.isNotBlank() && secondName.matches(correctName)
+                && adress.isNotBlank()
+                && place.isNotBlank() && zip.isNotBlank() && zip.matches(correctPlz)
+            ) {
+                viewModel.deleteAllCartItems()
+                showAlert()
+                findNavController().navigate(R.id.homeFragment)
+            } else {
+                if (firstName.isBlank() || !firstName.matches(correctName)) {
+                    Toast.makeText(requireContext(), "please fill a correct first name", Toast.LENGTH_SHORT)
+                        .show()
+                } else if (secondName.isBlank() || !secondName.matches(correctName)) {
+                    Toast.makeText(
+                        requireContext(),
+                        "please fill a correct second name",
+                        Toast.LENGTH_SHORT)
+                        .show()
+
+                } else if (zip.isBlank() || !zip.matches(correctPlz)) {
+                    Toast.makeText(requireContext(), "please fill a coorect zip",Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(requireContext(), "please fill all fields!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(requireContext())
+        //builder.setView(R.layout.) NICHT VERGESSEN ZU IMPLEMENTIERT
+        builder.setTitle("Vielen Dank für Ihre Bestellung!")
+        builder.setMessage("Sie erhalten per E-Mail eine Bestätigung.")
+
+        builder.setPositiveButton("OK") { dialog, which ->
+            dialog.dismiss()
+
+            // Positive Button Click Logic
+        }
+//        builder.setNegativeButton("Abbrechen") { dialog, which ->
+//            // Negative Button Click Logic
+//            dialog.dismiss()
+//        }
+        builder.show()
     }
 }
